165d9203e2dd7123b424405629bf34d52b3d7040
Scott Lobdell <scott.lobdell@gmail.com>
2025-01-20 16:05:07 -0800

    add updates to build script

--- sbl_build.sh
+++ sbl_build.sh
@@ -1,3 +1,7 @@
+# sudo apt-get install gcc-arm-none-eabi make
+# sudo apt-get install python3-empy
+# sudo apt-get install python3-future
+# sudo apt-get install python3-intelhex  # necessary for building .hex bootloader
 # git submodule update --init --recursive
 BOARD=FlywooF745
 BOARD=CubeOrangePlus
